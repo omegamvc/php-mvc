f321f5b4d01b0c5ce93b132e013121d037e1864f
omegamvc <agisoftt@gmail.com>
2025-07-18 22:25:52 +0200

    Some fix in project path

--- config/app.php
+++ config/app.php
@@ -1,20 +1,11 @@
 <?php
 
 return [
-    'BASEURL'           => env('BASEURL', 'http://localhost'),
-    'TIMEZONE'          => env('TIME_ZONE', ''),
-    'APP_KEY'           => env('APP_KEY', ''),
-    'ENVIRONMENT'       => env('APP_ENV', 'dev'),
-    'APP_DEBUG'         => (bool) env('APP_DEBUG', ''),
-
-    'BCRYPT_ROUNDS'     => (bool) env('BCRYPT_ROUNDS', 12),
-    'CONFIG_STORAGE'    => env('file', 'file'),
-
-    'PROVIDERS'         => [
-        App\Providers\AppServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\DatabaseServiceProvider::class,
-        App\Providers\ViewServiceProvider::class,
-        App\Providers\CacheServiceProvider::class,
-    ],
+    'BASEURL'        => env('BASEURL', 'http://localhost'),
+    'timezone'       => env('TIME_ZONE', 'UTC'),
+    'APP_KEY'        => env('APP_KEY', ''),
+    'ENVIRONMENT'    => env('APP_ENV', 'dev'),
+    'APP_DEBUG'      => (bool) env('APP_DEBUG', ''),
+    'BCRYPT_ROUNDS'  => (bool) env('BCRYPT_ROUNDS', 12),
+    'CONFIG_STORAGE' => env('file', 'file'),
 ];
